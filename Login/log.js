033668a4537f874f35ead7603c387df36c17f6e2
Diego-OM03 <odiegoorellana492@gmail.com>
2025-11-20 11:23:02 -0400

    update 5

--- Login/log.js
+++ Login/log.js
@@ -55,11 +55,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // Redirigir según el rol
       if (rol === "admin") {
-        window.location.href = "../dashboard/admin/admin.html";
+        window.location.href = "../dashboard/admin/index.html";
       } else if (rol === "medico") {
-        window.location.href = "../dashboard/medico/medico.html";
+        window.location.href = "../dashboard/medico/index.html";
       } else {
-        window.location.href = "../dashboard/pacientes/paciente.html";
+        window.location.href = "../dashboard/pacientes/index.html";
       }
 
     } catch (error) {
@@ -121,11 +121,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // Redirigir según el rol
       if (rol === "admin") {
-        window.location.href = "../dashboard/admin/admin.html";
+        window.location.href = "../dashboard/admin/index.html";
       } else if (rol === "medico") {
-        window.location.href = "../dashboard/medico/medico.html";
+        window.location.href = "../dashboard/medico/index.html";
       } else {
-        window.location.href = "../dashboard/pacientes/paciente.html";
+        window.location.href = "../dashboard/pacientes/index.html";
       }
 
     } catch (error) {
